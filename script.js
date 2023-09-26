e00af6e4f76e0dfc96916b246940b9b898cfaf9f
birajnior <ubiratanbernardo@gmail.com>
2023-09-26 18:21:44 -0300

    Finalizando JS

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+AOS.init({
+  duration: 1500,
+  once: true,
+});
+const banner2 = document.querySelector(".app__image-figure");
+
 const html = document.querySelector("html");
 const btnFoco = document.querySelector(".app__card-button--foco");
 const btnDescansoCurto = document.querySelector(".app__card-button--curto");
@@ -5,6 +11,7 @@ const btnDescansoLongo = document.querySelector(".app__card-button--longo");
 const banner = document.querySelector(".app__image");
 const title = document.querySelector(".app__title");
 const botoes = document.querySelectorAll(".app__card-button");
+const tempoNaTela = document.querySelector("#timer");
 const btnStartPause = document.querySelector(".app__card-primary-button");
 const iniciarOuPausarBtIcone = document.querySelector(
   ".app__card-primary-butto-icon"
@@ -16,7 +23,7 @@ const playSom = new Audio("sons/play.wav");
 const pauseSom = new Audio("sons/pause.mp3");
 const tempoFinalizado = new Audio("sons/beep.mp3");
 
-let tempoDecorridoEmSegundos = 5;
+let tempoDecorridoEmSegundos = 1500;
 let intervaloId = null;
 
 musica.loop = true;
@@ -29,21 +36,24 @@ musicaFocoInput.addEventListener("change", () => {
 });
 
 function alterarCor(contexto) {
+  mostrarTempo();
   botoes.forEach(function (contexto) {
     contexto.classList.remove("active");
   });
   html.setAttribute("data-contexto", contexto);
-  banner.setAttribute("src", `/imagens/${contexto}.png`);
   switch (contexto) {
     case "foco":
-      title.innerHTML = `Otimize sua produtividade,<br>
-      <strong class="app__title-strong">mergulhe no que importa.</strong>`;
+      title.innerHTML = `<h1  data-aos="fade-right">Otimize sua produtividade,<br>
+      <strong class="app__title-strong">mergulhe no que importa.</strong></h1>`;
+      banner2.innerHTML = `<img class="app__image" src="/imagens/${contexto}.png" alt="" data-aos="fade-up">`;
       break;
     case "descanso-curto":
-      title.innerHTML = `Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>`;
+      title.innerHTML = `<h1  data-aos="fade-down-right">Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong></h1>`;
+      banner2.innerHTML = `<img class="app__image" src="/imagens/${contexto}.png" alt="" data-aos="fade-left">`;
       break;
-    case "descanso-long":
-      title.innerHTML = `Hora de voltar à superfície.<strong class="app__title-strong">Faça uma pausa longa.</strong>`;
+    case "descanso-longo":
+      title.innerHTML = `<h1  data-aos="flip-left">Hora de voltar à superfície.<strong class="app__title-strong"> Faça uma pausa longa.</strong></h1>`;
+      banner2.innerHTML = `<img class="app__image" src="/imagens/${contexto}.png" alt="" data-aos="flip-right">`;
       break;
     default:
       break;
@@ -51,29 +61,32 @@ function alterarCor(contexto) {
 }
 
 btnFoco.addEventListener("click", () => {
+  tempoDecorridoEmSegundos = 1500;
   alterarCor("foco");
   btnFoco.classList.add("active");
 });
 
 btnDescansoCurto.addEventListener("click", () => {
+  tempoDecorridoEmSegundos = 300;
   alterarCor("descanso-curto");
   btnDescansoCurto.classList.add("active");
 });
 
 btnDescansoLongo.addEventListener("click", () => {
+  tempoDecorridoEmSegundos = 900;
   alterarCor("descanso-longo");
   btnDescansoLongo.classList.add("active");
 });
 
 const contagemRegressiva = () => {
   if (tempoDecorridoEmSegundos <= 0) {
-    zerar();
     tempoFinalizado.play();
     alert("Tempo finalizado!");
+    zerar();
     return;
   }
   tempoDecorridoEmSegundos -= 1;
-  console.log("Temporizador: " + tempoDecorridoEmSegundos);
+  mostrarTempo();
 };
 
 function iniciarOuPausar() {
@@ -96,3 +109,14 @@ function zerar() {
 }
 
 btnStartPause.addEventListener("click", iniciarOuPausar);
+
+function mostrarTempo() {
+  const tempo = new Date(tempoDecorridoEmSegundos * 1000);
+  const tempoFormatado = tempo.toLocaleTimeString("pt-br", {
+    minute: "2-digit",
+    second: "2-digit",
+  });
+  tempoNaTela.innerHTML = `${tempoFormatado}`;
+}
+
+mostrarTempo();
